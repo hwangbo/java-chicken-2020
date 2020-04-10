d8a936bbe1dfb437cb29f596b5602549e554f833
hwangbo <taekwonod714@naver.com>
2020-04-10 15:24:52 +0900

    feat: Bill 클래스 내 예외처리 기능 구현

--- src/main/java/domain/Bill.java
+++ src/main/java/domain/Bill.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bill {
+    private static final int CASH = 2;
+    private static final int TEN = 10;
+    private static final double DISCOUNT = 0.9;
+    public static final String INVALID_MENU_ERROR_MESSAGE = "메뉴의 값이 null입니다.";
+    public static final String INVALID_COUNT_ERROR_MESSAGE = "갯수는 1 이상 99 이하까지 가능합니다.";
+    public static final String INVALID_TOTAL_COUNT_ERROR_MESSAGE = "한 테이블이 주문할 수 있는 최대 메뉴 갯수는 99개입니다.";
+    public static final String INVALID_PAYMENT_ERROR_MESSAGE = "계산 수단이 잘못 입력되었습니다.";
+
     private Map<Menu, Integer> bill;
 
     public Bill() {
@@ -11,7 +19,17 @@ public class Bill {
     }
 
     public void order(Menu menu, int count) {
+        checkValidMenu(menu);
+        checkValidCount(count);
+        checkCountOver99(count);
         this.bill.put(menu, count);
+    }
+
+    private int totalCount() {
+        return bill.keySet()
+                .stream()
+                .mapToInt(menu -> bill.get(menu))
+                .sum();
     }
 
     public Map<Menu, Integer> getBill() {
@@ -25,19 +43,44 @@ public class Bill {
                 .mapToInt(menu -> bill.get(menu))
                 .sum();
 
-        return chickenCount >= 10;
+        return chickenCount >= TEN;
     }
 
     public double totalPrice(int payment) {
+        checkValidPayment(payment);
         int totalPrice = bill.keySet().stream().mapToInt(menu -> menu.price(bill.get(menu))).sum();
 
-        if (payment == 2) {
-            return totalPrice * 0.9;
+        if (payment == CASH) {
+            return totalPrice * DISCOUNT;
         }
         return totalPrice;
     }
 
+    private void checkValidPayment(int payment) {
+        if (payment != 1 && payment != 2) {
+            throw new IllegalArgumentException(INVALID_PAYMENT_ERROR_MESSAGE);
+        }
+    }
+
     public boolean isEmpty() {
         return this.bill.isEmpty();
+    }
+
+    private void checkValidMenu(Menu menu) {
+        if (menu == null) {
+            throw new IllegalArgumentException(INVALID_MENU_ERROR_MESSAGE);
+        }
+    }
+
+    private void checkValidCount(int count) {
+        if (count < 1 || count > 99) {
+            throw new IllegalArgumentException(INVALID_COUNT_ERROR_MESSAGE);
+        }
+    }
+
+    private void checkCountOver99(int count) {
+        if (count + totalCount() > 99) {
+            throw new IllegalArgumentException(INVALID_TOTAL_COUNT_ERROR_MESSAGE);
+        }
     }
 }
--- src/test/java/domain/BillTest.java
+++ src/test/java/domain/BillTest.java
@@ -2,16 +2,49 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BillTest {
     private static List<Menu> menus = MenuRepository.menus();
 
-    @DisplayName("주문한 치킨이 10마리가 넘는지 확인하는 기능 구현")
+    @DisplayName("주문시 메뉴가 null인 경우 예외 처리 테스트")
+    @Test
+    public void order_1() {
+        Bill bill = new Bill();
+        assertThatThrownBy(() -> bill.order(null, 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Bill.INVALID_MENU_ERROR_MESSAGE);
+    }
+
+    @DisplayName("주문시 숫자가 범위 밖일 경우 예외 처리 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 100})
+    public void order_2(int count) {
+        Bill bill = new Bill();
+        assertThatThrownBy(() -> bill.order(menus.get(0), count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Bill.INVALID_COUNT_ERROR_MESSAGE);
+    }
+
+    @DisplayName("주문시 주문 갯수가 100개 이상일 경우 예외 처리 테스트")
+    @Test
+    public void order_3() {
+        Bill bill = new Bill();
+        assertThatThrownBy(() -> {
+            bill.order(menus.get(0), 50);
+            bill.order(menus.get(1), 50);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Bill.INVALID_TOTAL_COUNT_ERROR_MESSAGE);
+    }
+
+    @DisplayName("주문한 치킨이 10마리가 넘는지 확인하는 기능 테스트")
     @Test
     public void isOverTenChickens() {
         Bill bill = new Bill();
@@ -25,9 +58,23 @@ class BillTest {
         assertTrue(bill.isOverTenChickens());
     }
 
-    @DisplayName("현금으로 결제 시, 총합의 10% 할인하는 기능 구현")
+    @DisplayName("계산 수단이 잘못 입력되었을 경우 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 3, 5, 99})
+    public void totalPrice_1(int payment) {
+        assertThatThrownBy(() -> {
+            Bill bill = new Bill();
+
+            bill.order(menus.get(0), 1);
+
+            bill.totalPrice(payment);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Bill.INVALID_PAYMENT_ERROR_MESSAGE);
+    }
+
+    @DisplayName("현금으로 결제 시, 총합의 10% 할인하는 기능 테스트")
     @Test
-    public void payWithCash() {
+    public void totalPrice_2() {
         Bill bill = new Bill();
 
         bill.order(menus.get(0), 1);
